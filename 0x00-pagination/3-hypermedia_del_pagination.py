2f7446cfa02b104d622bfb526416aa9d04a36d70
tewedaj <tewedaj13@gmail.com>
2023-02-20 21:00:37 +0300

    * fix buges / add requirements

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,16 +1,15 @@
-
 #!/usr/bin/env python3
-""" 3. Deletion-resilient hypermedia pagination
 """
-
+Deletion-resilient hypermedia pagination
+"""
 
 import csv
 import math
-from typing import List, Dict, Any
+from typing import List
 
 
 class Server:
-    """ Server class to paginate a database of popular baby names.
+    """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -19,7 +18,7 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """ Cached dataset
+        """Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -30,7 +29,7 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """ Dataset indexed by sorting position, starting at 0
+        """Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
@@ -41,6 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        pass
         """ Returns a dictionary.
         """
         assert type(index) == int
