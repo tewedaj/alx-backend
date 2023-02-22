89ded7b8711ef4972e786629a0d81ea75f1fbc11
tewedaj <tewedaj13@gmail.com>
2023-02-22 22:11:56 +0300

    * bouns task lfu_cache added

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -8,7 +8,7 @@ BaseCaching = __import__("base_caching").BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ class FIFOCache that inherits from BaseCaching and is a caching system
+    """ class that inherits from BaseCaching and is a caching system
     """
 
     def __init__(self):
